be5a4b542b66c117e699ed2388af02d7842b5a96
Marco Porcho <maporcho@gmail.com>
2019-06-18 23:06:03 -0300

    Showing different questions
    Keeping track of right and wrong answers

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  int questionNumber = 0;
+
+  List<bool> correctAnswers = [false, true, true];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +49,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -48,6 +60,7 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         Expanded(
+          flex: 1,
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
@@ -62,11 +75,25 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  scoreKeeper.add(correctAnswers[questionNumber]
+                      ? Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                      : Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+
+                  questionNumber = ++questionNumber % questions.length;
+                });
               },
             ),
           ),
         ),
         Expanded(
+          flex: 1,
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
@@ -80,11 +107,26 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  scoreKeeper.add(!correctAnswers[questionNumber]
+                      ? Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        )
+                      : Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ));
+
+                  questionNumber = ++questionNumber % questions.length;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
